5ccc89cce823d15da2b3c0c7dce42077fbad11d8
Vanboizz <102446711+Vanboizz@users.noreply.github.com>
2023-01-12 20:44:11 +0700

    Show hide pass

--- src/pages/ForgotPassword/index.jsx
+++ src/pages/ForgotPassword/index.jsx
@@ -1,8 +1,31 @@
 import styles from "./ForgotPassword.module.scss";
 import background from '../../assets/scenery.png';
 import hotel from '../../assets/hotel.png';
-
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 export default function ForgotPassword() {
+    const [typePassword, setTypePassword] = useState("password")
+    const [inputPassword, setInputPassword] = useState("")
+    const [typeConfirmPassword, setTypeConfirmPassword] = useState("password")
+    const [inputConfirmPassword, setInputConfirmPassword] = useState("")
+
+    const handleChange = () => {
+        if (typePassword === "password") {
+            setTypePassword("text")
+        } else {
+            setTypePassword("password")
+        }
+    }
+
+    const handleChangeType = () => {
+        if (typeConfirmPassword === "password") {
+            setTypeConfirmPassword("text")
+        } else {
+            setTypeConfirmPassword("password")
+        }
+    }
     return (
         <div className={styles.forgotPassword}>
             <div className={styles.left}>
@@ -13,21 +36,46 @@ export default function ForgotPassword() {
                 <h2>WELCOME</h2>
                 <form action="" className={styles.form}>
                     <div>
-                        <input id="trick1" style={{fontSize:"2rem"}} className={styles.inputOne} placeholder=" " type="text" name="staffid" required />
-                        <label for="trick1" style={{fontSize:"1.6rem"}} className={styles.labelOne}>STAFFID
+                        <input id="trick1" style={{ fontSize: "2rem" }} className={styles.inputOne} placeholder=" " type="text" name="staffid" required />
+                        <label for="trick1" style={{ fontSize: "1.6rem" }} className={styles.labelOne}>STAFFID
                         </label>
                     </div>
                     <div className={styles.password}>
-                        <input id="trick2" style={{fontSize:"2rem"}} className={styles.inputOne} placeholder=" " type="password" name="password" required />
-                        <label for="trick2"  style={{fontSize:"1.6rem"}} className={styles.labelOne}>NEW PASSWORD
+                        <input id="trick2" style={{ fontSize: "2rem" }} className={styles.inputOne} placeholder=" " type={typePassword} value={inputPassword} name="password" required onChange={(e) => { setInputPassword(e.target.value) }} />
+                        <label for="trick2" style={{ fontSize: "1.6rem" }} className={styles.labelOne}>NEW PASSWORD
                         </label>
                     </div>
                     <div className={styles.reTypePassword}>
-                        <input id="trick3" style={{fontSize:"2rem"}} className={styles.inputOne} placeholder=" " type="password" name="password" required />
-                        <label for="trick3"  style={{fontSize:"1.6rem"}} className={styles.labelOne}>RETYPE PASSWORD
+                        <input id="trick3" style={{ fontSize: "2rem" }} className={styles.inputOne} placeholder=" " type={typeConfirmPassword} name={typePassword} value={inputConfirmPassword} required onChange={(e) => { setInputConfirmPassword(e.target.value) }} />
+                        <label for="trick3" style={{ fontSize: "1.6rem" }} className={styles.labelOne}>RETYPE PASSWORD
                         </label>
                     </div>
                 </form>
+                <button onClick={handleChange}>
+                    {
+                        typePassword === "password" ? (
+                            <FontAwesomeIcon icon={faEyeSlash} style={{ position: "absolute", right: "7.5%", top: "61%" }} />
+
+                        ) :
+                            (
+                                <FontAwesomeIcon icon={faEye} style={{ position: "absolute", right: "7.5%", top: "61%" }} />
+
+                            )
+                    }
+                </button>
+
+                <button onClick={handleChangeType}>
+                    {
+                        typeConfirmPassword === "password" ? (
+                            <FontAwesomeIcon icon={faEyeSlash} style={{ position: "absolute", right: "7.5%", top: "71%" }} />
+
+                        ) :
+                            (
+                                <FontAwesomeIcon icon={faEye} style={{ position: "absolute", right: "7.5%", top: "71%" }} />
+
+                            )
+                    }
+                </button>
                 <button className={styles.btnConfirm}>CONFIRM</button>
             </div>
         </div>
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,8 +6,12 @@ import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const { ipcRenderer } = require("electron");
-
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faEyeSlash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Login() {
+    const [typePassword, setTypePassword] = useState("password")
+    const [inputPassword, setInputPassword] = useState("")
     const [checkEmail, setCheckEmail] = useState("");
     const [checkPass, setCheckPass] = useState("");
     const navigate = useNavigate();
@@ -25,17 +29,17 @@ export default function Login() {
         };
 
         fetch("https://hammerhead-app-7qhnq.ondigitalocean.app/api/login", requestOptions)
-        .then(response => response.json())
-        .then(res => {
-            if(res.user) {
-                ipcRenderer.send("save-token", res.token, res.user)
-                typeof(res) === "object" ? navigateFilter(res.user.role) : res;
-            }
-            else {
-                toast("Login failed!");
-            }
-        })
-        .catch((err) => console.log(err));
+            .then(response => response.json())
+            .then(res => {
+                if (res.user) {
+                    ipcRenderer.send("save-token", res.token, res.user)
+                    typeof (res) === "object" ? navigateFilter(res.user.role) : res;
+                }
+                else {
+                    toast("Login failed!");
+                }
+            })
+            .catch((err) => console.log(err));
     };
 
     const navigateFilter = (role) => {
@@ -52,6 +56,13 @@ export default function Login() {
         }
     }
 
+    const handleChange = () => {
+        if (typePassword === "password") {
+            setTypePassword("text")
+        } else {
+            setTypePassword("password")
+        }
+    }
     return (
         <div className={styles.login}>
             <div className={styles.left}>
@@ -62,22 +73,34 @@ export default function Login() {
                 <h2>WELCOME</h2>
                 <form action="" className={styles.form}>
                     <div>
-                        <input id="trick" style={{fontSize:"2rem"}} className={styles.inputOne} placeholder=" " type="text" name="staffid" required onChange={(e) => setCheckEmail(e.target.value)}/>
-                        <label for="trick" style={{fontSize:"1.6rem"}} className={styles.labelOne}>EMAIL
+                        <input id="trick" style={{ fontSize: "2rem" }} className={styles.inputOne} placeholder=" " type="text" name="staffid" required onChange={(e) => setCheckEmail(e.target.value)} />
+                        <label for="trick" style={{ fontSize: "1.6rem" }} className={styles.labelOne}>EMAIL
                         </label>
                     </div>
                     <div className={styles.password}>
-                        <input id="chich" style={{fontSize:"2rem"}} className={styles.inputOne} placeholder=" " type="password" name="password" required onChange={(e) => setCheckPass(e.target.value)}/>
-                        <label for="chich"  style={{fontSize:"1.6rem"}} className={styles.labelOne}>PASSWORD
+                        <input id="chich" style={{ fontSize: "2rem" }} className={styles.inputOne} placeholder=" " type={typePassword} value={inputPassword} required onChange={(e) => { setCheckPass(e.target.value); setInputPassword(e.target.value) }} />
+                        <label for="chich" style={{ fontSize: "1.6rem" }} className={styles.labelOne}>PASSWORD
                         </label>
                     </div>
                 </form>
+                <button onClick={handleChange}>
+                    {
+                        typePassword === "password" ? (
+                            <FontAwesomeIcon icon={faEyeSlash} style={{ position: "absolute", right: "6%", top: "62%" }} />
+
+                        ) :
+                            (
+                                <FontAwesomeIcon icon={faEye} style={{ position: "absolute", right: "6%", top: "62%" }} />
+
+                            )
+                    }
+                </button>
                 <div>
                     <a className={styles.forgotPassword} href="/forgotPassword">FORGOT PASSWORD?</a>
                 </div>
                 <div>
                     <button className={styles.btn} onClick={authenication}>LOGIN</button>
-                    <ToastContainer hideProgressBar={true}/>
+                    <ToastContainer hideProgressBar={true} />
                 </div>
             </div>
         </div>
